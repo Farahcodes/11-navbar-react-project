3beb7f2ecbc10a0aec80656c303bbec952be9f47
farah <farah.fournat@gmail.com>
2022-01-17 12:10:35 +0100

    conditionally showing the link container

--- setup/src/Navbar.js
+++ setup/src/Navbar.js
@@ -15,8 +15,8 @@ const Navbar = () => {
           </button>
         </div>
 
-        {showLinks &&
-        <div className="links-container show-container">
+        
+        <div className=  {`${showLinks? 'links-container show-container': 'links-container'}`} >
           <ul className="links">
             {links.map((link)=>{
               const {id, url, text}= link;
@@ -28,7 +28,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
-        }
+        
         <ul className="social-icons">
           {social.map((socialIcon)=>{
             const{id, url, icon}= socialIcon;
